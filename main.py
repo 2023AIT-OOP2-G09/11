35c21a3fbe3e730ebbf9b9ab2c51637203c6111c
shiruku-shibata <k22065kk@aitech.ac.jp>
2023-12-29 00:23:45 +0900

    画面表示完成版との連結

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from flask import Flask, render_template, request, redirect, url_for,send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from pathlib import Path
 import os
-import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-from moduel.cannycontour import apply_canny_edge_detection
-from moduel.face_cascade import detect_and_draw_faces
-from moduel.grayscale import grayscale_and_threshold
-from moduel.mozaiku import apply_mosaic_to_faces
-from pathlib import Path
+from module.cannycontour import apply_canny_edge_detection
+from module.face_cascade import detect_and_draw_faces
+from module.grayscale import grayscale_and_threshold
+from module.mozaiku import apply_mosaic_to_faces
 import cv2
 
 app = Flask(__name__)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 path_to_watch = os.path.join(current_dir, 'uploads')
-
 
 # アップロードされた画像の保存先ディレクトリ
 UPLOAD_FOLDER = 'uploads'
@@ -27,47 +25,39 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+# ファイルシステムの変更を監視し、変更があったら特定の処理を実行するハンドラ
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         if event.is_directory:
             # ディレクトリが変更された場合の処理
             print(f'Directory modified: {event.src_path}')
-            # ここに実行したいプログラムのコードを追加
-            apply_canny_edge_detection()
-            detect_and_draw_faces()
-            grayscale_and_threshold()
-            apply_mosaic_to_faces()
+            
+            # 画像処理を実行
+            apply_canny_edge_detection(UPLOAD_FOLDER, self.canny_folder)
+            detect_and_draw_faces(UPLOAD_FOLDER, self.drawface_folder)
+            grayscale_and_threshold(UPLOAD_FOLDER, self.grayscale_folder)
+            apply_mosaic_to_faces(UPLOAD_FOLDER, self.mosaic_folder)
+
+            # Flaskアプリケーションの設定を更新
+            app.config['Canny_FOLDER'] = str(self.canny_folder)
+            app.config['drawface_FOLDER'] = str(self.drawface_folder)
+            app.config['grayscale_FOLDER'] = str(self.grayscale_folder)
+            app.config['mosaic_FOLDER'] = str(self.mosaic_folder)
 
 # Flaskアプリケーション外でObserverオブジェクトを生成
 event_handler = MyHandler()
 observer = Observer()
 observer.schedule(event_handler, path=path_to_watch, recursive=True)
 
-
+# ホームページ
 @app.route('/')
 def index():
-    #path_to_watch = 'uploads'
-
-    #event_handler = MyHandler()
-    #observer = Observer()
-    #observer.schedule(event_handler, path=path_to_watch, recursive=True)
-
     print(f"Watching directory: {path_to_watch}")
-
-    #try:
-        # 監視を開始
-    #    observer.start()
-    #    while True:
-    #        time.sleep(1)
-    #except KeyboardInterrupt:
-        # Ctrl+Cが押されたら終了
-    #    observer.stop()
-    #observer.join()
     return render_template('index.html')
-   
+
+# 画像アップロード処理
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    # フォームから送信されたファイルを取得
     file = request.files['file']
 
     # ファイルが選択されていない場合の処理
@@ -87,121 +77,87 @@ def upload_file():
     # 許可されていない拡張子の場合の処理
     return '許可されていないファイル形式です。'
 
+# 画像一覧表示
 @app.route('/applist')
 def applist():
     # uploadsディレクトリ内の画像ファイル名を取得
     filenames = [filename for filename in os.listdir(os.path.join(current_dir, 'uploads')) if allowed_file(filename)]
     return render_template('applist_form.html', filenames=filenames)
 
+# アップロードされた画像の表示
 @app.route('/apppic/<filename>')
 def app_pic(filename):
-    # uploads ディレクトリから画像を返す
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
 
 # 処理済み画像の保存先ディレクトリ
 PROCESSED_FOLDER_GRAY = 'grayscale_image'
 app.config['PROCESSED_FOLDER_GRAY'] = PROCESSED_FOLDER_GRAY 
 
+# グレースケール画像表示
 @app.route('/gray')
 def gray():
-    # grayscale_image ディレクトリ内の画像ファイル名を取得
     gray_dir = os.path.join(current_dir, PROCESSED_FOLDER_GRAY)
     filenames = [filename for filename in os.listdir(os.path.join(current_dir, 'grayscale_image')) if allowed_file(filename)]
     return render_template('grayscale_form.html', filenames=filenames)
 
+# グレースケール画像の表示
 @app.route('/graypic/<filename>')
 def gray_pic(filename):
-    # grayscale_img ディレクトリから画像を返す
     return send_from_directory(app.config['PROCESSED_FOLDER_GRAY'], filename)
 
 # 処理済み画像の保存先ディレクトリ
 PROCESSED_FOLDER_MOZAIKU = 'mozaiku_image'
 app.config['PROCESSED_FOLDER_MOZAIKU'] = PROCESSED_FOLDER_MOZAIKU 
 
+# モザイク画像表示
 @app.route('/mozaiku')
 def mozaiku():
-    # grayscale_image ディレクトリ内の画像ファイル名を取得
     mozaiku_dir = os.path.join(current_dir, PROCESSED_FOLDER_MOZAIKU)
     filenames = [filename for filename in os.listdir(os.path.join(current_dir, 'mozaiku_image')) if allowed_file(filename)]
     return render_template('mozaiku_form.html', filenames=filenames)
 
-class MyHandler(FileSystemEventHandler):
-    def __init__(self, upload_folder):
-        # アップロードされた画像の保存先ディレクトリ
-        self.upload_folder = Path(upload_folder)
-
-        # それぞれの処理結果を保存するディレクトリ
-        self.canny_folder = self.upload_folder / "canny"
-        self.drawface_folder = self.upload_folder / "drawface"
-        self.grayscale_folder = self.upload_folder / "grayscale"
-        self.mosaic_folder = self.upload_folder / "mosaic"
-
-        # フォルダが存在しない場合は作成する
-        self.canny_folder.mkdir(exist_ok=True)
-        self.drawface_folder.mkdir(exist_ok=True)
-        self.grayscale_folder.mkdir(exist_ok=True)
-        self.mosaic_folder.mkdir(exist_ok=True)
-    def on_modified(self, event):
-        if event.is_directory:
-            # ディレクトリが変更された場合の処理
-            print(f'Directory modified: {event.src_path}')
-            # ここに実行したいプログラムのコードを追加
-            apply_canny_edge_detection(UPLOAD_FOLDER, self.canny_folder)
-            detect_and_draw_faces(UPLOAD_FOLDER, self.drawface_folder)
-            grayscale_and_threshold(UPLOAD_FOLDER, self.grayscale_folder)
-            apply_mosaic_to_faces(UPLOAD_FOLDER, self.mosaic_folder)
-
-            app.config['Canny_FOLDER'] = str(self.canny_folder)
-            app.config['drawface_FOLDER'] = str(self.drawface_folder)
-            app.config['grayscale_FOLDER'] = str(self.grayscale_folder)
-            app.config['mosaic_FOLDER'] = str(self.mosaic_folder)
-
+# モザイク画像の表示
 @app.route('/mozaikupic/<filename>')
 def mozaiku_pic(filename):
-    # grayscale_img ディレクトリから画像を返す
     return send_from_directory(app.config['PROCESSED_FOLDER_MOZAIKU'], filename)
 
 # 処理済み画像の保存先ディレクトリ
 PROCESSED_FOLDER_CASCADE = 'CASCADE_image'
 app.config['PROCESSED_FOLDER_CASCADE'] = PROCESSED_FOLDER_CASCADE
 
+# カスケード画像表示
 @app.route('/cascade')
 def cascade():
-    # grayscale_image ディレクトリ内の画像ファイル名を取得
     cascade_dir = os.path.join(current_dir, PROCESSED_FOLDER_CASCADE)
     filenames = [filename for filename in os.listdir(os.path.join(current_dir, 'cascade_image')) if allowed_file(filename)]
     return render_template('cascade_form.html', filenames=filenames)
 
+# カスケード画像の表示
 @app.route('/cascadepic/<filename>')
 def cascade_pic(filename):
-    # grayscale_img ディレクトリから画像を返す
     return send_from_directory(app.config['PROCESSED_FOLDER_CASCADE'], filename)
 
 # 処理済み画像の保存先ディレクトリ
 PROCESSED_FOLDER_CANNY = 'canny_image'
 app.config['PROCESSED_FOLDER_CANNY'] = PROCESSED_FOLDER_CANNY 
 
+# カニー画像表示
 @app.route('/canny')
 def canny():
-    # grayscale_image ディレクトリ内の画像ファイル名を取得
     canny_dir = os.path.join(current_dir, PROCESSED_FOLDER_CANNY)
     filenames = [filename for filename in os.listdir(os.path.join(current_dir, 'canny_image')) if allowed_file(filename)]
     return render_template('canny_form.html', filenames=filenames)
 
+# カニー画像の表示
 @app.route('/cannypic/<filename>')
 def canny_pic(filename):
-    # grayscale_img ディレクトリから画像を返す
     return send_from_directory(app.config['PROCESSED_FOLDER_CANNY'], filename)
 
-#if __name__ == "__main__":
-#    app.run(debug=True)
+# Flaskアプリケーションの起動
 if __name__ == "__main__":
     try:
-        # Flaskアプリケーションの起動
         app.run(debug=True, use_reloader=False)
     except KeyboardInterrupt:
         # Ctrl+Cが押されたらObserverを停止
         observer.stop()
     observer.join()
-    
